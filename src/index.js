b74f712a2d7c6f103fe1b5ac318229d60c7b62ff
Frederico Teixeira <frederico.teixeira@clearly.ca>
2021-01-19 11:20:33 -0800

    dependencies

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express =  require('express');
+const app =  express();
+const port =  process.env.PORT  ||  8080
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ nome:  'Dpendabot test' });
+});
+
+let server = app.listen(port, () => {
+    console.log(`Application running on ${port}`);
+});
+module.exports  = server;
